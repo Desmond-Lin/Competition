2fa93c06d338906667e0f2ba4cf1d66a7793cbe3
Desmond <yctreese@gmail.com>
2020-02-26 11:56:15 +0800

    full feature

--- wuli_sklearn_api.py
+++ wuli_sklearn_api.py
@@ -85,25 +85,29 @@ def f_eng(df, event_df, is_train=True):
                   on='event_id')
     # 当前hit和当前event统计值作差
     df['t_minus_terror'] = df['t'] - df['terror']
+    df['t_plus_terror'] = df['t'] + df['terror']
     df['q_minus_mean'] = df['q'] - df['q'].groupby(df['event_id']).transform(np.mean)
     df['t_minus_mean'] = df['t'] - df['t'].groupby(df['event_id']).transform(np.mean)
+
     # 当前hit和当前事件触发hit的距离
     df['x_minus_xcmc'] = df['x'] - df['xcmc']
     df['y_minus_ycmc'] = df['y'] - df['ycmc']
-    df['dis'] = np.sqrt(df['x_minus_xcmc'] ** 2 + df['y_minus_ycmc'] ** 2 + (df['t'] * 29.98) ** 2)
+    df['dis'] = np.sqrt(df['x_minus_xcmc'] ** 2 + df['y_minus_ycmc'] ** 2 )
+    df['dis_3dim'] = np.sqrt(df['x_minus_xcmc'] ** 2 + df['y_minus_ycmc'] ** 2 + (df['t'] * 29.98*100) ** 2)
 
     # 按照公式，拟合簇射前峰面
     l = np.sin(df['thetamc']) * np.cos(df['phimc'])
     m = np.sin(df['thetamc']) * np.sin(df['phimc'])
-    df['surface_fitting'] = df['t'] - l * df['x'] - m * df['y']
+    df['surface_fitting'] = df['t'] - (l * df['x'] + m * df['y'])/0.2998
 
-    # 原初粒子的能量和次级粒子电量的比例关系，min-max数据标准化放大差距
+    # 原初粒子的能量和次级粒子电量的比例关系，min-max数据标准化放大差距.q为负电荷全部都是信号，不知道什么原因
     df['q_divide_energymc'] = df['q'] / df['energymc']
     df['q_divide_energymc'] = (df['q_divide_energymc'] - df['q_divide_energymc'].min()) / (
                 df['q_divide_energymc'].max() - df['q_divide_energymc'].min())
 
-    # x_std, x_mean, x_skew, y_std, y_mean, y_skew, t_std, t_skew, q_quantile, q_min, q_skew, q_mean
+    # 测试后留下来的统计特征x_std, x_mean, x_skew, y_std, y_mean, y_skew, t_std, t_skew, q_quantile, q_min, q_skew, q_mean
     # get_stati_feature(df,'x')
+
     feature = ['x_std', 'x_mean', 'x_skew', 'y_std', 'y_mean', 'y_skew', 't_std', 't_skew', 'q_quantile', 'q_min',
                'q_skew', 'q_mean']
     for fea in feature:
@@ -116,12 +120,12 @@ def f_eng(df, event_df, is_train=True):
         df = df.drop('flag', axis=1)
     df = df.drop(['hit_id', 'z', 'event_id'], axis=1)
 
-    # isolation forest先预测一下，预测结果作为一个特征输入
-    rng = np.random.RandomState(42)
-    clf = IsolationForest(behaviour='new', max_samples=100,
-                          random_state=rng, contamination='auto')
-    clf.fit(df)
-    df['iso_pre'] = clf.decision_function(df)
+    # # isolation forest先预测一下，预测结果作为一个特征输入
+    # rng = np.random.RandomState(42)
+    # clf = IsolationForest(behaviour='new', max_samples=100,
+    #                       random_state=rng, contamination='auto')
+    # clf.fit(df)
+    # df['iso_pre'] = clf.decision_function(df)
 
     return df
 
@@ -131,8 +135,8 @@ train_df = f_eng(train_df_source, event_df_source)
 test_df = f_eng(test_df_source, event_df_source, False)
 print(train_df.columns.values.tolist())
 # 小样本训练
-train_df = train_df.iloc[300000:500000, :]
-labels = labels[300000:500000]
+# train_df = train_df.iloc[:500000, :]
+# labels = labels[:500000]
 
 train_x, val_x, train_y, val_y = train_test_split(train_df, labels, test_size=0.33, random_state=42)
 
@@ -172,18 +176,18 @@ def score(labels, pred):
     # check = [1 if x==y else 0 for x, y in zip(pred, labels)]
     # print("accuracy: {}".format(sum(check)/len(check)))
 
-    w1 = 0.5
-    w2 = 0.5
+    w1 = 0.1
+    w2 = 0.9
     metric = w1 * remaining + w2 * exemption
-    return 'score', metric, True
-
+    # return 'score', metric, True
+    return metric
 
 print(
     '=============================================== training and validati ===============================================')
 fea_imp_list = []
 clf = LGBMClassifier(
     learning_rate=0.01,
-    n_estimators=20000,
+    n_estimators=6000,
     num_leaves=255,
     subsample=0.9,
     colsample_bytree=0.8,
@@ -196,17 +200,17 @@ print('************** training **************')
 clf.fit(
     train_x, train_y,
     eval_set=[(val_x, val_y)],
-    eval_metric=score,
+    eval_metric='logloss',
     # categorical_feature=cate_cols,
-    early_stopping_rounds=200,
+    # early_stopping_rounds=200,
     # early_stopping_rounds=50,
     verbose=50
 )
 print('runtime:', time.time() - t)
 
-print('************** train and predict **************')
+print('************** save result **************')
 best_rounds = clf.best_iteration_
-best_score = clf.best_score_['valid_0']['score']
+best_score = clf.best_score_['valid_0']['binary_logloss']
 val_pred = clf.predict_proba(val_x)[:, 1]
 fea_imp_list.append(clf.feature_importances_)
 print('runtime:', time.time() - t)
@@ -227,9 +231,9 @@ print('************** training **************')
 clf.fit(
     train_df, labels,
     eval_set=[(train_df, labels)],
-    eval_metric=score,
+    eval_metric='logloss',
     # categorical_feature=cate_cols,
-    early_stopping_rounds=200,
+    # early_stopping_rounds=200,
     # early_stopping_rounds=50,
     verbose=50
 )
@@ -255,22 +259,22 @@ print(
     '=============================================== threshold search ===============================================')
 # f1阈值敏感，所以对阈值做一个简单的迭代搜索。
 # t0 = 0.05
-# v = 0.002
+# v = 0.01
 # best_t = t0
 # best_f1 = 0
-# for step in range(475):
+# for step in range(95):
 #     curr_t = t0 + step * v
 #     y = [1 if x >= curr_t else 0 for x in val_pred]
 #     curr_f1 = score(val_y, y)
 #     if curr_f1 > best_f1:
 #         best_t = curr_t
 #         best_f1 = curr_f1
-#         print('step: {}   best threshold: {}   best score: {}'.format(step, best_t, best_f1))
+#     print('curr_t: {}   best threshold: {}   best score: {}'.format(curr_t, best_t, best_f1))
 # print('search finish.')
 
-# val_pred = [1 if x >= best_t else 0 for x in val_pred]
-print('\nbest score:', best_score)
-# print('best score:', score(val_y, val_pred))
+val_pred = [1 if x >= threshold else 0 for x in val_pred]
+print('\nbest logloss:', best_score)
+print('best score:', score(val_y, val_pred))
 print('validate mean:', np.mean(val_pred))
 print('runtime:', time.time() - t)
 
@@ -284,5 +288,7 @@ sub.to_csv(
     index=False)
 print('runtime:', time.time() - t)
 logging.info('Best score: {}'.format(best_score))
+print('flag == 1 count: {}'.format(sub['hit_id'][sub['flag_pred']==1].count()))
+print('flag == 1 count: {}'.format(sub['hit_id'][sub['flag_pred']==1].count()))
 print('finish.')
 print('========================================================================================================')
